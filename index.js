40f89ef6d90966de9aaa75dd858e184158820b29
Fernando <fernando.isdicoder@gmail.com>
2021-11-18 16:58:55 +0100

    Added connection function to data base

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+const connectDB = require("./database");
 const { initializeServer } = require("./server");
 
 const port = process.env.PORT ?? process.env.SERVER_PORT ?? 5000;
 
-initializeServer(port);
+(async () => {
+  await connectDB();
+  initializeServer(port);
+})();
